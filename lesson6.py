2a8e3427a75adcaf27dd8f36556208250cfb2ef5
Kyrgyzbaev ADILET <98408321+ADIKOLORD@users.noreply.github.com>
2022-07-15 23:24:38 +0600

    Update lesson6.py

--- lesson6.py
+++ lesson6.py
@@ -1,4 +1,4 @@
-adafafafdddaaaffafaaafaaafaaaaaffaaaaffaaafaa
+adafafafdddaaaffafaaafaaafaaaaaffaaaaffaaafaaf
 """
 db = ['alf', 'fjadsk', 3, 4, 5, 65, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 while True:
